9470e6eb824e834ffabfa5ff2f4634f9167aba75
Jerlingit <jerlinthangam24@gmail.com>
2022-02-19 13:28:43 +0530

    jerlin

---
+++ src/test/java/org/test/GitTest/Test.java
@@ -0,0 +1,21 @@
+package org.test.GitTest;
+
+public class Test {
+
+	
+	public void test1() {
+		System.out.println("Test1");
+	}
+	
+	public void test2() {
+		System.out.println("Test1");
+	}
+	
+	public void test3() {
+		System.out.println("Test1");
+	}
+	
+	public void test4() {
+		System.out.println("Test1");
+	}
+}
